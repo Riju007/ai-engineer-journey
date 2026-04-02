7e12daf9cadea6712235ee07effc1b060891adbb
AVISEK DEY <avisekdey1994@gmail.com>
2026-04-02 22:47:03 +0530

    Adede SD manual streaming code

--- phase-1-ml-foundations/P1-house-price-predictor/standard_deviation_v2.py
+++ phase-1-ml-foundations/P1-house-price-predictor/standard_deviation_v2.py
@@ -2,34 +2,34 @@ from math import sqrt
 
 
 class RunningState(object):
-    data = []
+
+    def __init__(self):
+        self.count = 0
+        self.sum_x = 0
+        self.sum_x_2 = 0
 
     def add(self, num):
-        self.data.append(num)
-        return None
+        self.count += 1
+        self.sum_x += num
+        self.sum_x_2 += num**2
 
     def get_std(self):
-        data_list = self.data
-        len_data = len(data_list)
-        list_sum = sum(data_list)
-        mean = list_sum / len_data
-        print(f"mean is: {mean}")
-        list_sum_square = sum(list(map(lambda x: x ** 2, data_list)))
-        mean_square = mean ** 2
-        print(mean_square)
-        print(list_sum_square)
-        variance = (list_sum_square / len_data) - (mean ** 2)
-        print(variance)
+        if self.count == 0:
+            raise ValueError("There is no value")
+        mean = self.sum_x / self.count
+        mean_square = mean**2
+        variance = (self.sum_x_2 / self.count) - mean_square
+        variance = max(variance, 0.0)
         sd = sqrt(variance)
         return sd
 
 
 if __name__ == "__main__":
     obj = RunningState()
+    # obj.get_std()
     obj.add(2)
     obj.add(4)
     obj.add(6)
-    # obj.add(8)
-    print(f"data list is :{obj.data}")
+    obj.add(8)
     sd = obj.get_std()
     print(f"SD is: {sd}")
